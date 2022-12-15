c9645741fbc5eac00d15b1d57c8151b8ef3db0cc
MickyOr <mickyor97@gmail.com>
2022-12-15 15:20:44 -0400

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxElf = 0;
+        var curElf = 0;
+        var lastEmpty = false;
+        for (line in input) {
+            if (line.isEmpty()) {
+                maxElf = maxElf.coerceAtLeast(curElf);
+                curElf = 0;
+                lastEmpty = true;
+            }
+            else {
+                curElf += line.toInt();
+                lastEmpty = false;
+            }
+        }
+        assert(lastEmpty);
+        return maxElf;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var elves = mutableListOf<Int>();
+        var curElf = 0;
+        for (line in input) {
+            if (line.isEmpty()) {
+                elves.add(curElf);
+                curElf = 0;
+            }
+            else {
+                curElf += line.toInt();
+            }
+        }
+        elves.sortDescending();
+        return elves[0] + elves[1] + elves[2];
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
